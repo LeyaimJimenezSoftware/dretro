ac9dca8791d297c1863605828c7f65325d3414b5
Leyaim Jimenez <leyaim.jimenez.magmalabs.io>
2021-03-28 01:43:55 -0600

    Add new styles

---
+++ src/components/Card.js
@@ -0,0 +1,84 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCalendarAlt, faUser, faTag } from "@fortawesome/free-solid-svg-icons"
+import { COLORS } from "../constants/colors"
+import "./Card.css"
+
+const Card = ({ node, key }) => {
+  console.log("data node", node.categories.nodes[0].name)
+
+  return (
+    <div
+      key={key}
+      style={{
+        backgroundColor: "#ffffff",
+        marginTop: "20px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: "19px",
+        }}
+      >
+        <div id="imageDiv" className="img-card">
+          <Link to={node.slug}>
+            <img
+              id="content"
+              src={node.featuredImage.node.sourceUrl}
+              alt={`${node.slug}-img`}
+            />
+          </Link>
+        </div>
+        <div style={{ paddingTop: "10px" }}>
+          <Link to={node.slug}>
+            <h2 style={{ marginBottom: 0 }}>
+              <p
+                style={{
+                  fontSize: "24px",
+                  fontWeight: "bold",
+                  marginBottom: 0,
+                }}
+              >
+                {node.title}
+              </p>
+            </h2>
+          </Link>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <FontAwesomeIcon icon={faCalendarAlt} style={styles.cardStyle} />
+              <span className="card-info">{node.date}</span>
+            </div>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <FontAwesomeIcon icon={faUser} style={styles.cardStyle} />
+              <span className="card-info">{node.author.node.name}</span>
+            </div>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <FontAwesomeIcon icon={faTag} style={styles.cardStyle} />
+              <Link to={node.slug} style={{textDecoration: 'underline', marginTop: "1px"}} className="card-info">
+               {node.categories.nodes[0].name}
+              </Link>
+            </div>
+          </div>
+          <div
+            style={{ fontSize: "14px" }}
+            dangerouslySetInnerHTML={{ __html: node.excerpt }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
+
+const styles = {
+  cardStyle: {
+    color: COLORS.DANGO_PURPLE,
+    marginRight: 5,
+    alignSelf: "center",
+    fontSize: "12px",
+  },
+}
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+  RED: "#FF0000",
+  BLUE: "#0000FF",
+  GREEN: "#00FF00",
+  DANGO_PURPLE: "#4E52F8",
+};
